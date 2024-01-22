c993c282d0d0b92979e883401e012474c8eceb90
Ulku Firat Atlamaz <38175135+ATLAMAZ@users.noreply.github.com>
2024-01-22 14:48:49 -0500

    cnavas test added (#3)

--- e2e/tests/app.e2e.ts
+++ e2e/tests/app.e2e.ts
@@ -10,4 +10,20 @@ describe('App test', function () {
     expect(element).not.toBeNull();
 
   });
+  it('is running with canvas element', async function () {
+    const { page } = getBrowserState();
+
+    // Your existing test code...
+
+    // Check for the presence of the Angular webXR gallery element
+    const galleryElement = await page.locator('::-p-text(angular-webxr-gallery)').wait();
+    expect(galleryElement).not.toBeNull();
+
+    // New test command to check for a canvas element
+    const canvasElement = await page.locator('canvas').wait();
+
+    // Assertion for the canvas element
+    expect(canvasElement).not.toBeNull();
+  });
+
 });
