3d17c6225b7c5f82a7d1bf2e1dd1cfab0cb9ac62
Yönet <3523671+Yonet@users.noreply.github.com>
2024-05-05 16:23:32 +0300

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: "https://us-central1-imagenio.cloudfunctions.net/xr-gallery-py"
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: "https://us-central1-imagenio.cloudfunctions.net/xr-gallery-py"
-};
