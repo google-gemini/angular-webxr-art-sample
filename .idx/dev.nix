7bceb7857ee5845d407f5bee7d5ce183ed0f67e7
Fred Sauer <fredsa@google.com>
2024-05-15 17:33:58 -0700

    Add `.idx/dev.nix` file for Project IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,36 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.nodePackages.firebase-tools
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "angular.ng-template"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm install --no-audit --prefer-offline";
+      };
+      # To run something each time the environment is rebuilt, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" "--disable-host-check"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
